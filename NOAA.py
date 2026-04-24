16acd670c325f31231df435127c387d088e781d8
Joyce Liu <joliu2023@gmail.com>
2026-04-23 23:38:21 -0700

    saves retrieved data in csv files now, input now influences params

--- NOAA.py
+++ NOAA.py
@@ -4,17 +4,15 @@ import requests
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import date, timedelta
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import os
 
 
 class DataApp:
     def __init__(self, root):
         self.root = root
         self.root.title("NOAA Coastal Data Visualizer")
-        self.station_id = "9414290"
-        self.today = date.today()
-        print(self.today)
+        self.station_id = "9414523"
 
         self.df = pd.DataFrame()
         self.filtered_df = pd.DataFrame()
@@ -22,6 +20,7 @@ class DataApp:
         self.products = {
             "Water Temperature": "water_temperature",
             "Water Level": "water_level",
+            # "Hourly Height": "hourly_height",
             "Tide Predictions": "predictions",
         }
 
@@ -38,7 +37,7 @@ class DataApp:
         }
 
         tk.Label(root, text="Ocean Data Dashboard",
-                 font=("Arial", 30)).pack(pady=10)
+                 font=("Arial",)).pack(pady=10)
         control_frame = tk.Frame(root)
         control_frame.pack(pady=10)
 
@@ -54,9 +53,9 @@ class DataApp:
         )
         self.product_menu.grid(row=1, column=0, padx=8)
 
-        tk.Label(control_frame, text="Time range (hours)").grid(row=0, column=1, padx=5)
+        tk.Label(control_frame, text="Past number of days (max 31)").grid(row=0, column=1, padx=5)
 
-        self.time_range_var = tk.StringVar(value="24")
+        self.time_range_var = tk.StringVar(value="7")
         self.time_range_input = tk.Entry(
             control_frame,
             textvariable=self.time_range_var,
@@ -66,7 +65,7 @@ class DataApp:
 
         tk.Label(control_frame, text="Station").grid(row=0, column=2, padx=5)
 
-        self.station_var = tk.StringVar(value="San Francisco")
+        self.station_var = tk.StringVar(value="Redwood City")
         self.station_menu = ttk.Combobox(
             control_frame,
             textvariable=self.station_var,
@@ -86,7 +85,14 @@ class DataApp:
         )
         self.visualization_button.grid(row=1, column=4, padx=8)
 
-        self.figure, self.ax = plt.subplots(figsize=(7, 4))
+        # self.save_button = tk.Button(
+        #     control_frame, 
+        #     text="Save to CSV", 
+        #     command=self.save_to_csv
+        #     )
+        # self.save_button.grid(row=1, column=5, padx=8)
+
+        self.figure, self.ax = plt.subplots(figsize=(5, 3))
         self.canvas = FigureCanvasTkAgg(self.figure, master=root)
         self.canvas.get_tk_widget().pack(pady=10)
 
@@ -96,32 +102,69 @@ class DataApp:
         self.status_label = tk.Label(root, text="Ready")
         self.status_label.pack(pady=5)
 
+        self.data_folder = "noaa_data_csv_files"
+        if not os.path.exists(self.data_folder):
+            os.makedirs(self.data_folder)
+
     def get_data(self):
+        print("clicked get data")
         product = self.products[self.product_var.get()]
+        station_name = self.station_var.get().replace(" ", "_").lower()
+
+        days_back = int(self.time_range_var.get())
+        start_date = pd.to_datetime("today") - pd.Timedelta(days=days_back)
+        begin_date = start_date.strftime("%Y%m%d")
+        end_date = pd.to_datetime("today").strftime("%Y%m%d")
+
+        print(self.time_range_var.get())
+        print(begin_date)
+        print(end_date)
+
+
+        local_filename = f"{station_name}_{product}_{begin_date}_{end_date}.csv"
+        local_filepath = os.path.join(self.data_folder, local_filename)
+
+        if os.path.exists(local_filename):
+            print(f"Loading data from local file: {local_filepath}")
+            self.df = pd.read_csv(local_filepath)
+
+            self.df["time"] = pd.to_datetime(self.df["time"])
+            self.filtered_df = self.df
+            self.update_plot()
+            self.update_summary()
+            self.status_label["text"] = f"Found and loaded data from local file {local_filepath}"
+            return
+    
+
+        print("data not found locally, using api")
         url = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter"
         station = str(self.stations[self.station_var.get()])
 
         params = {
             "product": product,
             "application": "test",
-            "begin_date": "20260301",
-            "end_date": "20260302",
+            "begin_date": begin_date,
+            "end_date": end_date,
             "station": station,
             "time_zone": "gmt",
             "units": "metric",
             "format": "json"
         }
 
-        if product == "water_level":
-            params["datum"] = "MLLW"
-
-        if product == "predictions":
-            params["datum"] = "MLLW"
-            params["interval"] = "h"
+        if product in ["water_level", "hourly_height", "predictions"]:
+                params["datum"] = "MLLW"
+                if product == "predictions":
+                    params["interval"] = "h"
 
         try:
             response = requests.get(url, params=params)
             data = response.json()
+
+            # import json
+            # print(json.dumps(data, indent=4))
+            if "error" in data:
+                self.status_label["text"] = data["error"]["message"]
+                return
         except:
             self.status_label["text"] = "Error getting data"
             return
@@ -159,6 +202,8 @@ class DataApp:
         self.update_summary()
         self.status_label["text"] = "Ready"
 
+        self.save_to_csv()
+
     def update_plot(self):
         self.ax.clear()
 
@@ -189,6 +234,35 @@ class DataApp:
 
     def open_visualization_window(self):
         print("Visualization window clicked")
+
+    def save_to_csv(self):
+
+        if self.df.empty:
+            self.status_label["text"] = "No data to save"
+            return None
+    
+        data_type = self.products[self.product_var.get()]
+        station_name = self.station_var.get().replace(" ", "_").lower()
+
+
+        days_back = int(self.time_range_var.get())
+        start_date = pd.to_datetime("today") - pd.Timedelta(days=days_back)
+        begin_date = start_date.strftime("%Y%m%d")
+        end_date = pd.to_datetime("today").strftime("%Y%m%d")
+
+        filename = f"{station_name}_{data_type}_{begin_date}_{end_date}.csv"
+        filepath = os.path.join(self.data_folder, filename)
+
+        try:
+            self.df.to_csv(filepath,  index=False)
+            print(f"Successfully saved to {filename}")
+            self.status_label["text"] = f"Saved: {filename}"
+            return filename
+        except Exception as e:
+            print(f"Save failed: {e}")
+            self.status_label["text"] = "Save Error"
+            return None
+        
 
 
 if __name__ == "__main__":
